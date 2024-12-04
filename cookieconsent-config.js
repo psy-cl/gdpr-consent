a2dadcacee7fc144cda973cd468746f2df5d586b
psy-cl <psy@claudelefort.com>
2024-12-04 17:30:26 +0100

    Add files via upload

---
+++ cookieconsent-config.js
@@ -0,0 +1,77 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js';
+
+// Enable dark mode
+document.documentElement.classList.add('cc--darkmode');
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "box",
+            position: "bottom left",
+            equalWeightButtons: true,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "box",
+            position: "right",
+            equalWeightButtons: true,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        functionality: {},
+        analytics: {}
+    },
+    language: {
+        default: "fr",
+        autoDetect: "browser",
+        translations: {
+            fr: {
+                consentModal: {
+                    title: "Bonjour voyageur, c'est l'heure des cookies!",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    acceptAllBtn: "Tout accepter",
+                    acceptNecessaryBtn: "Tout rejeter",
+                    showPreferencesBtn: "Gérer les préférences",
+                    footer: "<a href=\"#link\">Politique de confidentialité</a>\n<a href=\"#link\">Termes et conditions</a>"
+                },
+                preferencesModal: {
+                    title: "Préférences de cookies",
+                    acceptAllBtn: "Tout accepter",
+                    acceptNecessaryBtn: "Tout rejeter",
+                    savePreferencesBtn: "Sauvegarder les préférences",
+                    closeIconLabel: "Fermer la modale",
+                    serviceCounterLabel: "Services",
+                    sections: [
+                        {
+                            title: "Utilisation des Cookies",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        },
+                        {
+                            title: "Cookies Strictement Nécessaires <span class=\"pm__badge\">Toujours Activé</span>",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Cookies de Fonctionnalités",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "functionality"
+                        },
+                        {
+                            title: "Cookies Analytiques",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "Plus d'informations",
+                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
